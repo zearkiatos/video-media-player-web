b793bdfed15774a6a4c37354bc3ee4499fd02b1e
zearkiatos <caprilespe@outlook.com>
2021-09-29 23:04:55 -0300

    First plugin and fix gitlab-ci

--- scripts/MediaPlayer.js
+++ scripts/MediaPlayer.js
@@ -1,9 +1,38 @@
 function MediaPlayer(configuration) {
   this.media = configuration.element;
+  this.plugins = configuration.plugins || [];
+
+  this.initializationPlugins();
 }
+
+MediaPlayer.prototype.initializationPlugins = function () {
+  this.plugins.forEach((plugin) => {
+    plugin.run(this);
+  });
+};
 
 MediaPlayer.prototype.play = function () {
   this.media.paused ? this.media.play() : this.media.pause();
+};
+
+MediaPlayer.prototype.pause = function () {
+  this.media.pause();
+};
+
+MediaPlayer.prototype.togglePlay = function () {
+  this.media.paused ? this.play() : this.pause();
+};
+
+MediaPlayer.prototype.mute = function () {
+  this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function () {
+  this.media.muted = false;
+};
+
+MediaPlayer.prototype.toggleMute = function () {
+  this.media.muted ? this.unmute() : this.mute();
 };
 
 export default MediaPlayer;
